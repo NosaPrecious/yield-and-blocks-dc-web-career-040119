82e06a1e0e05fe4eae5f3d829db5cb13ca4b1e81
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-19 07:34:41 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,8 @@
 def hello_t(array)
-  if block_given?
   count= 0
   while count < array.length
   yield array[count]
   count += 1
-  end
-      array
-    else
-      puts "Hey! No block was given!\n"
   end
 end
 
